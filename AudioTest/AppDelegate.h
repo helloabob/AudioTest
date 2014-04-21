5c3c8eb51ff7731a91f0f3329fd76b7f979f323b
helloabob <mac0001@mac0001s-MacBook-Air.local>
2014-04-21 11:53:33 +0800

    Initial Commit

---
+++ AudioTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioTest
+//
+//  Created by mac0001 on 4/21/14.
+//  Copyright (c) 2014 Bob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AudioTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AudioTest
+//
+//  Created by mac0001 on 4/21/14.
+//  Copyright (c) 2014 Bob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
